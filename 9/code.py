c47f2fdac09f39d19ec4e52c8a7c8038e41fddd0
RafalKornel <rafal.kornel123@gmail.com>
2019-03-20 21:46:48 +0100

    finished 9 riddle

--- 9/code.py
+++ 9/code.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3.5
 from blist import blist
+from collections import deque
+
 
 class Circle():
 
     def __init__(self, players_count):
-        self.marbles = blist([ 0 ])
-        self.current_marble = self.marbles[0]
+        self.marbles = deque([0])
         self.players_scores = [0] * players_count
 
 
     def add_marble(self, marble, player):
-        current_marble_index = self.marbles.index(self.current_marble)
-
         if marble % 23 != 0:
-            self.marbles.insert((current_marble_index + 2) % len(self.marbles), marble)
-            self.current_marble = marble
+            self.marbles.rotate(-2)
+            self.marbles.appendleft(marble)
 
         else:
-            self.players_scores[player] += marble + self.marbles[current_marble_index - 7]
-            self.current_marble = self.marbles[current_marble_index - 6]
-            self.marbles.pop(current_marble_index - 7)
+            self.marbles.rotate(7)
+            self.players_scores[player] += marble + self.marbles.popleft()
 
 
     def __str__(self):
@@ -39,5 +37,4 @@ while(cur_marble < last_marble):
     for pl in range(player_count):
         circle.add_marble(cur_marble, pl)
         cur_marble += 1
-    print(cur_marble)
 print(max(circle.players_scores))
